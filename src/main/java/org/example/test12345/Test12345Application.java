17a187a14606067da96c3ad1471cf31bb585b9a5
HaHa <2016113325@qq.com>
2024-12-10 14:06:01 +0800

    tag1

--- src/main/java/org/example/test12345/Test12345Application.java
+++ src/main/java/org/example/test12345/Test12345Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test12345Application {
 
     public static void main(String[] args) {
+        System.out.println("tag1");
         SpringApplication.run(Test12345Application.class, args);
     }
 
