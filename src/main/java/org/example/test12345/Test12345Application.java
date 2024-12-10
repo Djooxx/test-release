90d393afebb0b985d333ddd05abedb30c579d3e5
HaHa <2016113325@qq.com>
2024-12-10 13:58:13 +0800

    init

---
+++ src/main/java/org/example/test12345/Test12345Application.java
@@ -0,0 +1,13 @@
+package org.example.test12345;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test12345Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test12345Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/test12345/Test12345ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.test12345;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test12345ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
